5804720ac001c011e930fc8b8069c9ae603fb9c1
taketoiida <taketo.iida.e379@dirbato.co.jp>
2024-04-05 12:26:39 +0900

    sample0405_1226

--- sample0405/src/first/sample0405.java
+++ sample0405/src/first/sample0405.java
@@ -4,7 +4,10 @@ public class sample0405 {
 
     public static void main(String[] args) {
         System.out.println("Kiramex");
-        System.out.println("Java");
+        //this method works...
+        /*System.out.println("Java");
+         * 
+         */
         System.out.println("学習を始めましょう!");
     }
 
