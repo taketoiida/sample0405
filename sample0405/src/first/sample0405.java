0f0f1fa99bba13a48452a3c3a0286f03bb306e8f
taketoiida <taketo.iida.e379@dirbato.co.jp>
2024-04-05 12:22:31 +0900

    sample0405_1222

--- sample0405/src/first/sample0405.java
+++ sample0405/src/first/sample0405.java
@@ -3,8 +3,9 @@ package first;
 public class sample0405 {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
-
+        System.out.println("Kiramex");
+        System.out.println("Java");
+        System.out.println("学習を始めましょう!");
     }
 
 }
